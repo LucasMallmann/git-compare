a65b4228bfc6bb370e68a80d982e6ed5dd49d4be
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-05-06 19:16:30 -0300

    Instalando o styled-components e configurando o eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+
+import GlobalStyle from "./styles/global";
+
+const Title = styled.h1`
+  color: #f00;
+  font-size: 32px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <Title>Hello world</Title>
+    </Fragment>
   );
 }
 
