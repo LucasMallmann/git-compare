7b8e10d62e2918e7111d5e9c9e2e1e8e6d24c8bd
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-05-07 00:24:43 -0300

    Estilizando a lista de repositorios para se comparar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Fragment } from "react";
-import styled from "styled-components";
 
 import GlobalStyle from "./styles/global";
+import Main from "./pages/Main";
 
-const Title = styled.h1`
-  color: #f00;
-  font-size: 32px;
-`;
-
-function App() {
-  return (
-    <Fragment>
-      <GlobalStyle />
-      <Title>Hello world</Title>
-    </Fragment>
-  );
-}
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Main />
+  </Fragment>
+);
 
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import logo from "../../assets/logo.png";
+
+import { Container, Form } from "./styles";
+
+import CompareList from "../../components/CompareList";
+
+export default class Main extends Component {
+  state = {
+    repositories: []
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form>
+          <input type="text" placeholder="usuário/repositório" />
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
